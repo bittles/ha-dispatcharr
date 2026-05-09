cc7a3ee20b660725941f2ca7443cdc790bcc133f
bittles <bauern.sc@gmail.com>
2026-05-09 02:22:35 -0500

    Revert "try optionsflow"
    This reverts commit ab961e82400c2703952bf49c84b4c0f6727172e9.

--- custom_components/dispatcharr_sensor/config_flow.py
+++ custom_components/dispatcharr_sensor/config_flow.py
@@ -5,10 +5,6 @@ from typing import Any
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.data_entry_flow import FlowResult
-from homeassistant.helpers.schema_config_entry_flow import (
-    SchemaFlowFormStep,
-    SchemaOptionsFlowHandler,
-)
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
@@ -20,27 +16,13 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
         vol.Required("port", default=9191): int,
         vol.Required("username"): str,
         vol.Required("password"): str,
-        **OPTIONS_SCHEMA.schema,
     }
 )
-
-OPTIONS_SCHEMA = vol.Schema(
-    {
-        vol.Optional("enable_epg", default=true): bool
-    }
-)
-
-OPTIONS_FLOW = {
-    "init": SchemaFlowFormStep(OPTIONS_SCHEMA),
-}
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Dispatcharr Sensor."""
 
     VERSION = 1
-
-    def async_get_options_flow(config_entry: ConfigEntry) -> SchemaOptionsFlowHandler:
-        return SchemaOptionsFlowHandler(config_entry, OPTIONS_FLOW)
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
